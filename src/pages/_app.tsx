2b9562f16db6333136d7f2bcdf7c3bde60b0235b
Manuel Lara <smazzbass@gmail.com>
2023-07-04 18:50:12 -0600

    Added Navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,28 @@
 import { type AppType } from "next/app";
+import { ClerkProvider } from "@clerk/nextjs";
 import { api } from "y/utils/api";
 import "y/styles/globals.css";
+import NavBar from "y/components/navbar";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  
+  
+      return(
+        <ClerkProvider {...pageProps}>
+          <NavBar/>
+          <Component {...pageProps} />;
+          
+        </ClerkProvider>
+      ) 
+      
+          
+
 };
 
 export default api.withTRPC(MyApp);
+
+
+
+
+ 
+
